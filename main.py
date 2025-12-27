a0c5def3021a9242e698105597f5f6269a78a365
Jose Quiceno <josequiceno000@gmail.com>
2025-12-27 09:35:49 -0600

    feat(main): add basic introduction

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 
+user_attempts = 3
+
+def welcome():
+    print("Welcome to the number guessing game!")
+    print("Try to guess the number I'm thinking of [between 1 and 100].")
+    print(f"You have {user_attempts} attempts to guess the correct number.")
+
 def main():
-    print("Hello, World!")
+    welcome()
 
 if __name__ == "__main__":
     main()
