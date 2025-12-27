6819ad1063434edb963aa600465e0fc5dd592cdc
Jose Quiceno <josequiceno000@gmail.com>
2025-12-27 09:37:36 -0600

    feat(main): use random to select number

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import random
 
 user_attempts = 3
 
 def welcome():
+    chosen_number = random.randint(1, 100)
     print("Welcome to the number guessing game!")
     print("Try to guess the number I'm thinking of [between 1 and 100].")
     print(f"You have {user_attempts} attempts to guess the correct number.")
+
+    print(f"\nPssst, the correct number is {chosen_number} (just for testing purposes).")
 
 def main():
     welcome()
